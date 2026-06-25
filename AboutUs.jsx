b4a5081fbc7f208d20ac15a056c0e378796cb42b
tmchaudhary2005-design <tmchaudhary2005@gmail.com>
2026-06-25 21:38:01 +0530

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div className="about-us-container">
+      <h2>About Paradise Nursery</h2>
+      <p>Welcome to Paradise Nursery, your number one source for all things green...</p>
+      {/* Add more company details here */}
+    </div>
+  );
+};
+
+export default AboutUs;
